bbb2478e8787a659062948aec0d32983508d36c6
Krishtalbot <krishtalking33@gmail.com>
2024-01-17 21:38:44 +0545

    Make details more crisp

---
+++ terraingeneration.py
@@ -0,0 +1,378 @@
+import os
+import sys
+import ctypes
+import numpy as np
+import math
+import OpenGL.GL as gl
+import OpenGL.GLUT as glut
+from perlin_noise import PerlinNoise
+
+# Initializing Global Parameters
+noise = PerlinNoise()
+fieldOfView = 50
+byteOffset = 0
+perlinNoiseFactor = 0.12
+
+# Setting perlin Noise factor through Command Line Arguments
+if len(sys.argv) == 2:
+    perlinNoiseFactor = float(sys.argv[1])
+
+# Paramters [ Mesh Size, Screen Reoslution, Scaling Size, Perlin Factor]
+parameters = [48, 25, 8, 4, 4, perlinNoiseFactor]
+startPoint = 0
+
+# Vetex Shader Definition
+vertexShaderCode = """
+    attribute vec3 position;
+    uniform mat4 transformationMatrix[2];
+    uniform mat4 projectionMatrix;
+    uniform mat4 translate;
+    
+    void main(){
+        gl_Position = projectionMatrix * transformationMatrix[0] * transformationMatrix[1] * translate * vec4(position, 1.0);
+    }
+    """
+
+# Fragment Shader Definition
+fragmentShaderCode = """
+    void main(){
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    """
+
+# -- Building Data --
+def generateTerrain(value):
+    global data 
+    global noise
+    
+    data = []
+
+    halfDataX = int(value[0] * value[4] / 2)
+    halfDataY = int(value[1] * value[4] / 2)
+
+    scaledResolutionX = int(value[2] * value[4] / 2)
+    scaledResolutionY = int(value[3] * value[4] / 2)
+
+    startX = 0
+    startY = 0
+    for j in range(-halfDataY, halfDataY):
+        for i in range(-halfDataX, halfDataX):
+            iData = i / scaledResolutionX
+            jData = j / scaledResolutionY
+            nextiData = (i + 1) / scaledResolutionX
+            nextjData = (j + 1) / scaledResolutionY
+            data.append([iData, jData, mapNoise(noise([startX, startY]))])
+            data.append([iData, nextjData, mapNoise(noise([startX, (startY + value[5])]))])
+            data.append([nextiData, jData, mapNoise(noise([(startX + value[5]), startY]))])
+            startX = startX + value[5]
+        
+        startY = startY + value[5]
+        startX = 0
+
+    data = np.array(data, dtype = np.float32)
+    return data
+
+Animate function to create a dynamically flowing terrain through y-axis [called every 10 microsecond]
+def animate(offset):
+    global noise
+    global byteOffset
+    global parameters
+    global fieldOfView
+
+    offsetY = int(parameters[1] * parameters[4] / 2) + offset
+    
+    halfDataX = int(parameters[0] * parameters[4] / 2)
+
+    scaledResolutionX = int(parameters[2] * parameters[4] / 2)
+    scaledResolutionY = int(parameters[3] * parameters[4] / 2)
+
+    noOfData = 2 * halfDataX * 3
+
+    translation = generateTranslation([0.0, -offset/scaledResolutionY, 0.0])
+
+    loc = gl.glGetUniformLocation(program, "translate")
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_TRUE, translation)
+
+    projectionMatrix = generatePerspective()  
+
+    loc = gl.glGetUniformLocation(program, "projectionMatrix")
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_TRUE, projectionMatrix)
+
+    data = []
+    nextjData = (offsetY + 1) / scaledResolutionY
+    jData = offsetY / scaledResolutionY
+    startY = ((parameters[1] * parameters[4]) + offset) * parameters[5]
+    startX = 0
+    for i in range(-halfDataX, halfDataX):
+        iData = i / scaledResolutionX
+        nextiData = (i + 1) / scaledResolutionX
+        data.append([iData, jData, mapNoise(noise([startX, startY]))])
+        data.append([iData, nextjData, mapNoise(noise([startX, (startY + parameters[5])]))])
+        data.append([nextiData, jData, mapNoise(noise([(startX + parameters[5]), startY]))])
+        startX = startX + parameters[5]
+    data = np.array(data, dtype = np.float32)
+
+    rowBytes = noOfData * data.strides[0]
+    maxBuffer = int(parameters[1] * parameters[4]) -1 
+
+    # Set Byte Offset to 0 on reaching maximum bytes
+    if  byteOffset > maxBuffer:
+        byteOffset = 0
+
+    # Substitute the Buffer Data based on byte offset
+    gl.glBufferSubData(gl.GL_ARRAY_BUFFER, byteOffset * rowBytes, rowBytes, data)
+    
+    byteOffset = byteOffset + 1
+
+    # Call oneself every 10 microsecond
+    glut.glutTimerFunc(10, animate, offset + 1)
+
+# funtion to map noise values between Min and Max
+def mapNoise(noiseValue, maxMap = 1, minMap = 0.75):
+    if noiseValue > 0:
+        return noiseValue * maxMap
+    else:
+        return noiseValue * minMap
+
+# function to request and compiler shader slots from GPU
+def createShader(source, type):
+    # request shader
+    shader = gl.glCreateShader(type)
+
+    # set shader source using the code
+    gl.glShaderSource(shader, source)
+
+    gl.glCompileShader(shader)
+    if not gl.glGetShaderiv(shader, gl.GL_COMPILE_STATUS):
+        error = gl.glGetShaderInfoLog(shader).decode()
+        print(error)
+        raise RuntimeError(f"{source} shader compilation error")
+
+    return shader
+
+# function to build and activate program
+def createProgram(vertex, fragment):
+    program = gl.glCreateProgram()
+
+    # attach shader objects to the program
+    gl.glAttachShader(program, vertex)
+    gl.glAttachShader(program, fragment)
+
+    gl.glLinkProgram(program)
+    if not gl.glGetProgramiv(program, gl.GL_LINK_STATUS):
+        print(gl.glGetProgramInfoLog(program))
+        raise RuntimeError("Linking error")
+
+    # Get rid of shaders (no more needed)
+    gl.glDetachShader(program, vertex)
+    gl.glDetachShader(program, fragment)
+
+    return program
+
+# Rotation Matrix Generator
+def generateRotation(transformationData = None):
+    if not transformationData or transformationData[0] == "":
+        transformationMatrix = np.array(
+            [
+                1.0,0.0,0.0,0.0,
+                0.0,1.0,0.0,0.0,
+                0.0,0.0,1.0,0.0,
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+        return transformationMatrix
+    cTheta = np.cos(transformationData[1] / 180 * math.pi)
+    sTheta = np.sin(transformationData[1] / 180 * math.pi)
+
+    # x - axis rotation
+    if transformationData[0] == "pitch":
+        transformationMatrix = np.array(
+            [
+                1.0,0.0,0.0,0.0,
+                0.0,cTheta,-sTheta,0.0,
+                0.0,sTheta,cTheta,0.0,
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+
+    # y - axis rotation
+    elif transformationData[0] == "yaw":
+        transformationMatrix = np.array(
+            [
+                cTheta,0.0,sTheta,0.0,
+                0.0,1.0,0.0,0.0,
+                -sTheta,0.0,cTheta,0.0,
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+
+    # z - axis rotation
+    elif transformationData[0] == "roll":
+        transformationMatrix = np.array(
+            [
+                cTheta,-sTheta,0.0,0.0,
+                sTheta,cTheta,0.0,0.0,
+                0.0,0.0,1.0,0.0,
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+    
+    return transformationMatrix
+
+# Translation Matrix Generator
+def generateTranslation(translationData = None):
+    if not translationData or translationData[0] == "":
+        transformationMatrix = np.array(
+            [
+                1.0,0.0,0.0,0.0,
+                0.0,1.0,0.0,0.0,
+                0.0,0.0,1.0,0.0,
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+
+        return transformationMatrix
+
+    transformationMatrix = np.array(
+            [
+                1.0,0.0,0.0,translationData[0],
+                0.0,1.0,0.0,translationData[1],
+                0.0,0.0,1.0,translationData[2],
+                0.0,0.0,0.0,1.0,
+            ],
+            np.float32,
+        )
+
+    return transformationMatrix
+
+# Perspective Projection Generatior based on Field of View
+def generatePerspective():
+    global fieldOfView
+
+    fov = (fieldOfView * math.pi)/180
+    tanFOVHalf = np.tan(fov / 2.0)
+    f = 1/tanFOVHalf
+    projectionMatrix = np.array([f, 0.0, 0.0, 0.0,
+                                0.0, 1.0, 0.0, 0.0,
+                                0.0, 0.0, 1.0, 0.0,
+                                0.0, 0.0, 1.0, 1.0], dtype = np.float32)    
+    return projectionMatrix
+
+
+# initialization function
+def initialize():
+    global program
+    global data
+    global parameters
+    global vertexBuffer
+    global fieldOfView
+
+    # Enabling Depth Buffer Test
+    gl.glEnable(gl.GL_DEPTH_TEST)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    gl.glClearColor(0.0, 0.0, 0.0, 0.0)
+    gl.glLoadIdentity()
+
+    # Initializing the Global Program
+    program = createProgram(
+        createShader(vertexShaderCode, gl.GL_VERTEX_SHADER),
+        createShader(fragmentShaderCode, gl.GL_FRAGMENT_SHADER),
+    )
+    
+    # building data
+    data = generateTerrain(parameters)
+    
+    # generating projection, translation and rotation matrix
+    projectionMatrix = generatePerspective()
+    
+    translationMatrix =generateTranslation([0, 0.0, 0.3])
+    
+    translation = generateTranslation()
+    rotationMatrix = generateRotation(["pitch", 45])
+    
+    transformationMatrix = np.array([rotationMatrix, translationMatrix])
+    
+    # make program the default program
+    gl.glUseProgram(program)
+
+    # Generate Vexter Buffer
+    vertexBuffer = gl.glGenBuffers(1)
+
+    # Make this generated Buffer to be Array Buffer
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertexBuffer)
+
+    # Bind the position attribute
+    stride = data.strides[0]
+    offset = ctypes.c_void_p(0)
+    loc = gl.glGetAttribLocation(program, "position")
+    gl.glEnableVertexAttribArray(loc)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertexBuffer)
+    gl.glVertexAttribPointer(loc, 3, gl.GL_FLOAT, False, stride, offset)
+
+    # Bind the transformation, rotation and projection matrices to vertex shader's uniforms
+    loc = gl.glGetUniformLocation(program, "transformationMatrix")
+    gl.glUniformMatrix4fv(loc, 2, gl.GL_TRUE, transformationMatrix)
+
+    loc = gl.glGetUniformLocation(program, "projectionMatrix")
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_TRUE, projectionMatrix)
+
+    loc = gl.glGetUniformLocation(program, "translate")
+    gl.glUniformMatrix4fv(loc, 1, gl.GL_TRUE, translation)
+
+    # Upload data
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, data.nbytes, data, gl.GL_DYNAMIC_DRAW)
+
+
+def display():
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+    # Set Drwaing mode to Lines instead of Fill
+    gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+
+    # Draw Traingular Meshes
+    gl.glDrawArrays(gl.GL_TRIANGLES, 0, data.shape[0])
+    glut.glutSwapBuffers()
+    glut.glutPostRedisplay()
+
+def reshape(width, height):
+    gl.glViewport(0, 0, width, height)
+
+# Exit on Escape
+def keyboard(key, x, y):
+    if key == b"\x1b":
+        os._exit(1)
+
+# Track the change in Mouse Pointer on Drag to modify Field of View
+def mouse(x ,y):
+    global startPoint
+    global fieldOfView
+
+    delX = x - startPoint
+
+    if (delX == 0):
+        fieldOfView = 70
+    else:
+        fieldOfView = (fieldOfView + (delX * 0.0050)) % 180
+
+# GLUT init
+glut.glutInit()
+glut.glutInitDisplayMode(glut.GLUT_DOUBLE | glut.GLUT_RGBA | glut.GLUT_DEPTH)
+glut.glutCreateWindow("Graphics Window")
+glut.glutReshapeWindow(1920, 1080)
+glut.glutReshapeFunc(reshape)
+
+initialize()
+animate(0)
+
+glut.glutDisplayFunc(display)
+glut.glutPostRedisplay()
+glut.glutKeyboardFunc(keyboard)
+glut.glutMotionFunc(mouse)
+
+# enter the mainloop
+glut.glutMainLoop()
---
+++ test.py
@@ -0,0 +1,244 @@
+import noise
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+movement_speed = 0.4
+rotation_angle = 0.4
+
+class Vector(object):
+    def __init__(self, x, y, z=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def normalize(self):
+        length = np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        if length != 0:
+            self.x /= length
+            self.y /= length
+            self.z /= length
+
+scale = 15.0
+
+height_scale = 10
+
+octaves = 8
+octaveOffsets = []
+
+persistence = 0.5
+lacunarity = 2
+
+flattening_threshold = 0.0125
+
+for i in range(octaves):
+    octaveOffsets.append(Vector(np.random.randint(-10000, 10000), np.random.randint(-10000, 10000)))
+
+terrain_size = 200
+terrain = []
+offset = Vector(0, 0, 50)
+normals = np.zeros((terrain_size, terrain_size, 3), dtype=np.float32)
+
+def animate(_):
+    global offset
+    offset.y += movement_speed
+    calculate_terrain()
+    glutPostRedisplay()
+    glutTimerFunc(16, animate, 0)
+
+def calculate_terrain():
+    global terrain
+    terrain = []
+    for y in range(terrain_size):
+        terrain.append([])
+        for x in range(terrain_size):
+            terrain[-1].append(0)
+
+    for y in range(terrain_size):
+        for x in range(terrain_size):
+            amplitude = 1
+            frequency = 1
+            noiseHeight = 0
+            for i in range(octaves):
+                sampleX = frequency * (x + octaveOffsets[i].x + offset.x) / scale
+                sampleY = frequency * (y + octaveOffsets[i].y + offset.y) / scale
+                noiseHeight += amplitude * noise.pnoise2(sampleX, sampleY)
+                amplitude *= persistence
+                frequency *= lacunarity
+            terrain[x][y] = noiseHeight
+
+    min_val = np.min(terrain)
+    max_val = np.max(terrain)
+
+    terrain = np.array(terrain)
+    np.clip(terrain, -0.71, max_val)
+
+    calculate_normals()
+
+color_heights = [-0.7078, -0.6518, -0.5057, -0.27, -0.07, 0.1765, 0.3725, 0.5686, 0.9608]
+
+def calculate_normals():
+    global normals
+    normals = np.zeros((terrain_size, terrain_size, 3), dtype=np.float32)
+
+    for y in range(terrain_size):
+        for x in range(terrain_size):
+            dx = terrain[(x + 1) % terrain_size][y] - terrain[x][y]
+            dy = terrain[x][(y + 1) % terrain_size] - terrain[x][y]
+
+            normal = Vector(dy, -dx, 2.0)
+            normal.normalize()
+
+            normals[x][y] = [normal.x, normal.y, normal.z]
+
+def keyboard(bkey, x, y):
+    key = bkey.decode("utf-8")
+    if key == 'w':
+        offset.y += offset.z
+        calculate_terrain()
+    elif key == 'a':
+        offset.x -= offset.z
+        calculate_terrain()
+    elif key == 's':
+        offset.y -= offset.z
+        calculate_terrain()
+    elif key == 'd':
+        offset.x += offset.z
+        calculate_terrain()
+
+def initGL():
+    calculate_terrain()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glClearDepth(1.0)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glShadeModel(GL_SMOOTH)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, [1.0, 1.0, 1.0, 0.0])
+    glEnable(GL_COLOR_MATERIAL)
+    glColorMaterial(GL_FRONT, GL_DIFFUSE)
+
+def getColor(value):
+    global color_heights
+    if value < color_heights[0]:
+        return 42 / 255.0, 93 / 255.0, 186 / 255.0
+    elif color_heights[0] <= value < color_heights[1]:
+        return 51 / 255.0, 102 / 255.0, 195 / 255.0
+    elif color_heights[1] <= value < color_heights[2]:
+        return 207 / 255.0, 215 / 255.0, 127 / 255.0
+    elif color_heights[2] <= value < color_heights[3]:
+        return 91 / 255.0, 169 / 255.0, 24 / 255.0
+    elif color_heights[3] <= value < color_heights[4]:
+        return 63 / 255.0, 119 / 255.0, 17 / 255.0
+    elif color_heights[4] <= value < color_heights[5]:
+        return 89 / 255.0, 68 / 255.0, 61 / 255.0
+    elif color_heights[5] <= value < color_heights[6]:
+        return 74 / 255.0, 59 / 255.0, 55 / 255.0
+    elif color_heights[6] <= value < color_heights[7]:
+        return 250 / 255.0, 250 / 255.0, 250 / 255.0
+    elif value >= color_heights[7]:
+        return 1, 1, 1
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glTranslatef(-terrain_size / 2.0, -25, -6)
+    glRotate(60, -1, 0, 0)
+    glEnableClientState(GL_NORMAL_ARRAY)
+    glNormalPointer(GL_FLOAT, 0, normals)
+    offset.x += movement_speed
+    glBegin(GL_TRIANGLES)
+    for y in range(1, terrain_size - 1):
+        for x in range(1, terrain_size - 1):
+            if terrain[x][y + 1] < flattening_threshold:
+                glNormal3fv(normals[x][y + 1])
+                color = getColor(terrain[x][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex(x, (y + 1), 0)
+            else:
+                glNormal3fv(normals[x][y])
+                color = getColor(terrain[x][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f(x, (y + 1), (terrain[x][y + 1] * height_scale))
+
+            if terrain[x][y] < flattening_threshold:
+                color = getColor(terrain[x][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f(x, y, 0)
+            else:
+                color = getColor(terrain[x][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f(x, y, (terrain[x][y] * height_scale))
+
+            if terrain[x + 1][y + 1] < flattening_threshold:
+                glNormal3fv(normals[x + 1][y + 1])
+                color = getColor(terrain[x + 1][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), (y + 1), 0)
+            else:
+                color = getColor(terrain[x + 1][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), (y + 1), (terrain[x + 1][y + 1] * height_scale))
+    glEnd()
+
+    glBegin(GL_TRIANGLES)
+    for y in range(1, terrain_size - 1):
+        for x in range(1, terrain_size - 1):
+            if terrain[x + 1][y + 1] < flattening_threshold:
+                color = getColor(terrain[x + 1][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), (y + 1), 0)
+            else:
+                color = getColor(terrain[x + 1][y + 1])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), (y + 1), (terrain[x + 1][y + 1] * height_scale))
+
+            if terrain[x + 1][y] < flattening_threshold:
+                color = getColor(terrain[x + 1][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), y, 0)
+            else:
+                color = getColor(terrain[x + 1][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f((x + 1), y, (terrain[x + 1][y] * height_scale))
+
+            if terrain[x][y] < flattening_threshold:
+                color = getColor(terrain[x][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f(x, y, 0)
+            else:
+                color = getColor(terrain[x][y])
+                glColor3f(color[0], color[1], color[2])
+                glVertex3f(x, y, (terrain[x][y] * height_scale))
+    glEnd()
+    glutSwapBuffers()
+
+def reshape(width, height):
+    if height == 0:
+        height = 1
+    aspect = width / height
+
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+    gluPerspective(45.0, aspect, 0.1, 100.0)
+
+if __name__ == '__main__':
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(800, 600)
+    glutInitWindowPosition(50, 50)
+    glutCreateWindow("3D Terrain")
+    glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+    glutKeyboardFunc(keyboard)
+    initGL()
+    glutTimerFunc(0, animate, 0)
+
+    glutMainLoop()
